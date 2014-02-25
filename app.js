449d7d3ea99a419d706f41169d35b836c3c14100
Jorge Reyero <jorgereyero@gmail.com>
2014-02-25 23:47:17 +0000

    Extract wine logic from app file

--- app.js
+++ app.js
@@ -2,4 +2,13 @@
 var http = require('http');
 
 var express = require('express');
-var app = expres();
+var wines = require('./routes/wines.js');
+
+var app = express();
+
+app.get('/wines', wines.findAll);
+app.get('/wines/:id', wines.findById);
+
+app.listen(3000);
+
+console.log('Listening on port 3000');
