5de5d6fe2bc5e4da0f59547993b26610a58539c2
Jorge Reyero <jorgereyero@gmail.com>
2014-02-28 00:17:34 +0000

    Add mongodb and create de connection

--- routes/wines.js
+++ routes/wines.js
@@ -1,3 +1,23 @@
+var mongo = require('mongodb');
+
+var Server = mongo.Server,
+	Db = mongo.Db,
+	BSON = mongo.BSONPure;
+	
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+db = new Db('winedb', server);
+
+db.open(function(err, db){
+	if(!err){
+		console.log("Connected to the 'winedb' database");
+		db.collection('wines', {strict:true}, function(err, collection) {
+            if (err) {
+                console.log("The 'wines' collection doesn't exist. Creating it with sample data...");
+                populateDB();
+            }
+        });
+	}
+});
 
 exports.findAll = function(req, res){
 	res.send([{name:'wine1'}, {name:'wine2'}, {name:'wine3'}]);
