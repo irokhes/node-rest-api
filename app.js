bde39032b45b7da7c7cdb55a719122b0e1d949e0
Jorge Reyero <jorgereyero@gmail.com>
2014-02-28 00:33:45 +0000

    Modify app.js

--- app.js
+++ app.js
@@ -6,8 +6,16 @@ var wines = require('./routes/wines.js');
 
 var app = express();
 
+app.configure(function(){
+	app.use(express.logger('dev'));
+	app.use(express.bodyparser());
+});
+
 app.get('/wines', wines.findAll);
 app.get('/wines/:id', wines.findById);
+app.post('/wines', wine.addWine);
+app.put('/wines/:id', wine.updateWine);
+app.delete('/wines/:id', wine.deleteWine);
 
 app.listen(3000);
 
