6b12935306968f8417018caa598e3cdce4b403c8
Jorge Reyero <jorgereyero@gmail.com>
2014-03-01 13:21:30 +0000

    fix syntax error

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ var app = express();
 
 app.configure(function(){
 	app.use(express.logger('dev'));
-	app.use(express.bodyparser());
+	app.use(express.bodyParser());
 });
 
 app.get('/wines', wines.findAll);
 app.get('/wines/:id', wines.findById);
-app.post('/wines', wine.addWine);
-app.put('/wines/:id', wine.updateWine);
-app.delete('/wines/:id', wine.deleteWine);
+app.post('/wines', wines.addWine);
+app.put('/wines/:id', wines.updateWine);
+app.delete('/wines/:id', wines.deleteWine);
 
 app.listen(3000);
 
